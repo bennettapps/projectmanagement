d1d69d386968a80f428501e221aeb4311334ec38
Jesse Bennett <jesse@bennettapps.com>
2023-11-28 03:32:21 +0000

    move navbar and add dark mode

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -70,6 +70,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:name, :date, :complete, :overdue, :status)
+      params.require(:task).permit(:name, :date, :complete)
     end
 end
