c82fd81f9ca82b9d80ab6f7ca076060817f6dde3
Jesse Bennett <jesse@bennettapps.com>
2023-12-09 01:21:07 +0000

    final tweaks

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,10 +2,10 @@ class Project < ApplicationRecord
     has_many :tasks
 
     def completion_percentage
-        return tasks.where(complete: true).count * 100 / [tasks.count, 1].max
+        tasks.where(complete: true).count * 100 / [tasks.count, 1].max
     end
 
     def overdue_count
-        return tasks.select { |t| Date.today > Date.new(t.year, t.month, t.day) && !t.complete }.count
+        tasks.select { |t| Date.today > Date.new(t.year, t.month, t.day) && !t.complete }.count
     end
 end
